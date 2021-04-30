a26c58db4ca53355db9233222ed9495cdfd9d98d
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-04-30 13:40:28 -0700

    init commit

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,26 @@
+class PokemonController < ApplicationController
+  def index
+  end
+
+  def search
+    pokemon = find_pokemon(params[:number])
+
+    unless pokemon
+      flash[:alert] = 'Pokemon not found'
+      return render action: :index
+    end
+
+    @pokemon_name = pokemon["name"]
+  end
+
+
+  private
+  def request_api(url)
+    response = Excon.get(url)
+    return nil if response.status != 200
+    JSON.parse(response.body)
+  end
+  def find_pokemon(number)
+    request_api("https://pokeapi.co/api/v2/pokemon/#{number}")
+  end
+end
